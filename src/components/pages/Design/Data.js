bc3250341dc256b21f1d575d9e67b5d298f0b586
Villiss <lukas.vilim@outlook.com>
2021-06-30 12:32:09 +0200

    pomenene vela noveho

--- src/components/pages/Design/Data.js
+++ src/components/pages/Design/Data.js
@@ -2,25 +2,12 @@ export const homeObjThree= {
     lightBg: true,
     lightText: true,
     lightTextDesc: true,
-    topLine: 'DIZAJN',
-    headline: 'Stavba rodinných domov',
+    topLine: 'Stavba rodinných domov',
+    headline: 'Dizajn',
     description: 
-    `Každý dom je živý organizmus tak ako ľudia žijúci v ňom. Preto okrem technologických parametrov musí dobrý dom napĺňať túžby majiteľov na estetiku a prevádzku svojho bývania. Spojenie týchto chladne technických a pocitovo vnímajúcich aspektov vám vie najlepšie zabezpečiť architekt. Preto spolupracujeme s ateliérom APEX A s.r.o. ktorý navrhuje domy aké by projektovali sami pre seba. Využite ich skúsenosti s vašimi potrebami a možnosťami.Skladba steny ktorú požívame
-    1. Sádrovláknitá doska hrúbka 12,5 mm ( FERMACELL )
-    2. Paropriepustná fólia ( JUTAFOL )
-    3. Nosná konštrukcia – pozinkovaná oceľ hrúbka 120 mm ( LINDAB )
-    4. Akustická minerálna vlna hrúbka 160 mm ( KNAUF Decibel TI140)
-    5. Sádrovláknitá doska hrúbka 12,5 mm ( FERMACELL )
-    6. Difúzna fólia Lindab Contact Foil 95
-    7. Fasádna minerálna vlna hrúbka 100 mm ( KNAUF FKD-S )
-    8. Sklotextilná mriežka OMFA / VERTEX 145g
-    9. Silikónová omietka BAUMIT AT, PROHET
-    
-    Popis systému Linab Construline (pdf - 4,4MB)
-    Technická špecifikácia Lindab (pdf - 0,5MB)
-    Porovnanie Fermacell a Knauf (pdf - 1MB)`,
+    'Každý dom je živý organizmus tak ako ľudia žijúci v ňom. Preto okrem technologických parametrov musí dobrý dom napĺňať túžby majiteľov na estetiku a prevádzku svojho bývania. Spojenie týchto chladne technických a pocitovo vnímajúcich aspektov vám vie najlepšie zabezpečiť architekt. Preto spolupracujeme s ateliérom APEX A s.r.o. ktorý navrhuje domy aké by projektovali sami pre seba. Využite ich skúsenosti s vašimi potrebami a možnosťami.',
     buttonLabel: 'Kontaktujte nás',
     imgStart: '',
-    img: 'images/svg-1.svg',
+    img: 'images/svg-2.svg',
     alt: 'Credit Card'
 };
--- src/components/pages/Technologies/Technologies.js
+++ src/components/pages/Technologies/Technologies.js
@@ -2,6 +2,7 @@ import { motion } from 'framer-motion'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from '../../Button'
+import '../../Section.css'
 
 const pageVariants = {
     initial: {
@@ -41,10 +42,10 @@ const Technologies = () => {
                         <div className="col">
                             <div className="home__text-wrapper">
                                 <div className="top-line">
-                                TECHNOLÓGIA
+                                Stavba rodinných domov
                                 </div>
-                                <h1 className='heading'>Stavba rodinných domov</h1>
-                                <p className='home__subtitle dark'>Pri realizovaní stavieb neponechávame veci náhode. Preto používame montovaný stavebný systém od renomovanej medzinárodnej firmy <a href="http://www.lindab.com/sk/pro/pages/default.aspx?redirecttoproorhome=true&i=2685/">Lindab.</a><br /> 
+                                <h1 className='heading'>Technológia</h1>
+                                <p className='home__subtitle '>Pri realizovaní stavieb neponechávame veci náhode. Preto používame montovaný stavebný systém od renomovanej medzinárodnej firmy <a href="http://www.lindab.com/sk/pro/pages/default.aspx?redirecttoproorhome=true&i=2685/">Lindab.</a><br /> 
                                 Táto technológia nám umožňuje stavať rýchlo, jednoducho a v akýchkoľvek poveternostných podmienkach. Montovaná technológia využíva ľahkú oceľovú nosnú konštrukciu s výplňou minerálnej tepelnej izolácie. <br /> Ako plášť na fasádu a interér sú za podklad zvolené sádrovlaknité dosky s doplňujúcimi fóliami. Pre naplnenie nízkoenergetického štandardu pridávame z exteriérovej strany ďalšiu tepelnú izoláciu ochránenú kvalitnou silikónovou omietkou.<br/><br/></p>
                                 <Link to='/kontakt'>
                                     <Button buttonSize='btn--wide' buttonColor='blue'>Kontaktujte nás</Button>
@@ -54,7 +55,7 @@ const Technologies = () => {
                         <div className="col">
                         <h2>Skladba steny ktorú požívame</h2>
 
-                                    <ol>
+                                    <ol className='customlist'>
                                     <li>Sádrovláknitá doska hrúbka 12,5 mm ( FERMACELL )</li>
                                     <li>Paropriepustná fólia ( JUTAFOL )</li>
                                     <li>Nosná konštrukcia – pozinkovaná oceľ hrúbka 120 mm ( LINDAB )</li>
